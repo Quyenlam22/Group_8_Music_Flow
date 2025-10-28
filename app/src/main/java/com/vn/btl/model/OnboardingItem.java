13e24065a220a2503ac376905fb4c78c2a4a0d67
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-10-28 17:55:48 +0700

    Welcome

---
+++ app/src/main/java/com/vn/btl/model/OnboardingItem.java
@@ -0,0 +1,25 @@
+package com.vn.btl.model;
+
+public class OnboardingItem {
+    private int imageRes;
+    private String title;
+    private String description;
+
+    public OnboardingItem(int imageRes, String title, String description) {
+        this.imageRes = imageRes;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getImageRes() {
+        return imageRes;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ app/src/main/java/com/vn/btl/ui/activity/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.vn.btl.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.vn.btl.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new android.os.Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, OnboardingActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2000);
+    }
+}
