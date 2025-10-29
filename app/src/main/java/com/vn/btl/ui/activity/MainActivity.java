0b086fb7bad5e96b4eb1b1049b4adb4244c63904
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-10-29 09:14:31 +0700

    config firebase

--- app/src/main/java/com/vn/btl/ui/activity/MainActivity.java
+++ app/src/main/java/com/vn/btl/ui/activity/MainActivity.java
@@ -5,15 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import com.vn.btl.R;
 import com.vn.btl.ui.viewmodel.MainViewModel;
+import android.util.Log;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private MainViewModel viewModel;
+    private static final String TAG = "FirebaseTest";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseApp.initializeApp(this);
+        checkFirebaseLogin();
 
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
@@ -22,5 +30,25 @@ public class MainActivity extends AppCompatActivity {
         });
 
         viewModel.loadSongs();
+    }
+
+    private void checkFirebaseLogin() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if (currentUser != null) {
+            Log.d(TAG, "Đã đăng nhập Firebase với UID: " + currentUser.getUid());
+            return;
+        }
+
+        auth.signInAnonymously()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = auth.getCurrentUser();
+                        Log.d(TAG, "✅ Kết nối Firebase thành công! UID: " + user.getUid());
+                    } else {
+                        Log.e(TAG, "❌ Lỗi khi đăng nhập Firebase: ", task.getException());
+                    }
+                });
     }
 }
