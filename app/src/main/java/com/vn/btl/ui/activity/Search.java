4a44a340e9f9a667fde487e88696d65999e87177
PhamTung268 <tungnoluv2608@gmail.com>
2025-11-03 02:47:17 +0700

    UI search

--- app/src/main/java/com/vn/btl/ui/activity/Search.java
+++ app/src/main/java/com/vn/btl/ui/activity/Search.java
@@ -10,14 +10,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.vn.btl.R;
 
-public class MainActivity extends AppCompatActivity {
+public class Search extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_search);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.search), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
