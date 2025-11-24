8ff0d9df9242b13dd86f43f9f9c02aad1067fe8a
NgoTan <ngovantannvt04@gmail.com>
2025-11-25 00:17:58 +0700

    update

--- app/src/main/java/com/vn/btl/database/AppDatabase.java
+++ app/src/main/java/com/vn/btl/database/AppDatabase.java
@@ -7,24 +7,28 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.vn.btl.model.Artist;
+import com.vn.btl.model.FavoriteSong;
+import com.vn.btl.database.ArtistDAO;
+import com.vn.btl.database.FavoriteSongDAO;
 
-@Database(entities = {Artist.class}, version = 1, exportSchema = false)
+// THÊM FavoriteSong VÀO entities
+@Database(entities = {Artist.class, FavoriteSong.class}, version = 2, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     private static volatile AppDatabase INSTANCE;
 
     public abstract ArtistDAO artistDAO();
+    public abstract FavoriteSongDAO favoriteSongDAO();
 
     public static AppDatabase getInstance(Context context) {
         if (INSTANCE == null) {
             synchronized (AppDatabase.class) {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(
-                            context.getApplicationContext(),
-                            AppDatabase.class,
-                            "music_app_db"
-                    ).fallbackToDestructiveMigration()
-                     .fallbackToDestructiveMigrationOnDowngrade()
-                     .build();
+                                    context.getApplicationContext(),
+                                    AppDatabase.class,
+                                    "music_app_db"
+                            ).fallbackToDestructiveMigration()
+                            .build();
                 }
             }
         }
