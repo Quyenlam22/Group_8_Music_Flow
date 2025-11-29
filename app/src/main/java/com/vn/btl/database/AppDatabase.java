d58dd41aa1bb7cca49e8ec31173cb6406746b3d5
PhamTung268 <tungnoluv2608@gmail.com>
2025-11-29 23:01:14 +0700

    Update

--- app/src/main/java/com/vn/btl/database/AppDatabase.java
+++ app/src/main/java/com/vn/btl/database/AppDatabase.java
@@ -27,7 +27,7 @@ public abstract class AppDatabase extends RoomDatabase {
                             context.getApplicationContext(),
                             AppDatabase.class,
                             "music_app_db"
-                    ).fallbackToDestructiveMigration()
+                    )
                             .build();
                 }
             }
